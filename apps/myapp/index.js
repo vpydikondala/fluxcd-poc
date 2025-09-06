4c9fd87eea4d9618bce83d3c35111e1b79cc4424
Venkanna Pydikondala <venkanna.pydikondala@gmail.com>
2025-09-06 18:24:02 +0100

    updated index and package

--- apps/myapp/index.js
+++ apps/myapp/index.js
@@ -1,1 +1,12 @@
-console.log("Hello from myapp!");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 80;
+
+app.get('/', (req, res) => {
+  res.send('Hello from myapp - updated!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
